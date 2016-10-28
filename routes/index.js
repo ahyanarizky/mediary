1df6c19a9f4bda83b66483b9d59174455f62f84d
Septian A Tama <tama@tamatamvan.web.id>
2016-10-28 13:14:15 +0700

    index ok

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var controller = require('../controllers/indexController')
+var passport = require('passport')
+
+/* GET home page. */
+router.get('/', controller.viewIndex);
+
+// LOGIN FORM
+router.get('/login', controller.formLogin)
+
+// LOGIN PROCESS
+router.post('/login', passport.authenticate('local'), controller.processLogin)
+
+// REGISTER FORM
+router.get('/register', controller.formRegister)
+
+//REGISTER PROCESS
+router.post('/register', controller.proccessRegister)
+
+
+module.exports = router;
