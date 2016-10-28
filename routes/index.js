1cbf310e67c04d80756a00b964493c62dfb71ae7
Septian A Tama <tama@tamatamvan.web.id>
2016-10-28 13:31:32 +0700

    record for patients ok

--- routes/index.js
+++ routes/index.js
@@ -18,5 +18,12 @@ router.get('/register', controller.formRegister)
 //REGISTER PROCESS
 router.post('/register', controller.proccessRegister)
 
+router.get('/records', (req, res, next) => {
+  res.render('records')
+});
+
+router.get('/record_details', (req, res, next) => {
+  res.render('record_details')
+});
 
 module.exports = router;
